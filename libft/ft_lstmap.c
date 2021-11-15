c464079fa1524fd3144d4dbcf61051b0f5d72ffb
Thom <romanothomas98@gmail.com>
2021-11-15 14:43:50 +0100

    finito

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:21:51 by tromano           #+#    #+#             */
-/*   Updated: 2021/11/11 16:59:49 by tromano          ###   ########.fr       */
+/*   Updated: 2021/11/15 14:36:25 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 
-	new = malloc(sizeof(t_list) * ft_lstsize(lst));
-	if (!new)
+	if (!lst)
 		return (NULL);
-	ft_lstiter(new, (void *)(*f));
-	ft_lstiter(new, (*del));
+	new = ft_lstnew(f(lst->content));
+	while (lst->next)
+	{
+		new->next = ft_lstmap(lst->next, *(*f), *del);
+	}
 	return (new);
 }
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,82 +6,37 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 13:43:39 by tromano           #+#    #+#             */
-/*   Updated: 2021/11/11 17:44:17 by tromano          ###   ########.fr       */
+/*   Updated: 2021/11/15 14:43:08 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-
-int	ft_lenlen(char const *s1, char const *s2)
-{
-	int	i;
-	int	lens;
-
-	i = 0;
-	lens = 0;
-	while (i < ft_strlen(s1))
-	{
-		lens = lens + ft_strlen(s1);
-		i++;
-	}
-	lens = lens + ft_strlen(s1) + ft_strlen(s2) + 1;
-	return (lens);
-}
-
-char	*ft_strcat(char *dest, char *src)
-{
-	int	a;
-	int	b;
-
-	a = 0;
-	b = 0;
-	while (dest[b] != '\0')
-	{
-		b++;
-	}
-	while (src[a] != '\0')
-	{
-		dest[b] = src[a];
-		a++;
-		b++;
-	}
-	dest[b] = '\0';
-	return (dest);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
-	char	*str;
+	int		len1;
+	int		len2;
+	char	*s;
 
-	if (ft_strlen(s1) == 0)
-	{
-		str = malloc(sizeof(char));
-		return (str);
-	}
-	str = malloc(sizeof(char) * (ft_lenlen(s1, s2)));
-	if (!(str))
+	if (s1 || s2)
 		return (NULL);
-	*str = 0;
+	len1 = ft_strlen(s1);
+	len2 = ft_stllren(s2);
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
+		return (NULL);
 	i = 0;
-	if (i < ft_strlen(s1))
+	while (*s1)
 	{
-		ft_strcat(str, ((char *)s1));
-		if (i < ft_strlen(s1))
-			ft_strcat(str, ((char *)s2));
-		i++;
+		s[i++] = *s1;
+		s1++;
 	}
-	return (str);
+	while (*s2)
+	{
+		s[i++] = *s2;
+		s2++;
+	}
+	s[i] = '\0';
+	return (s);
 }
-
-// int	main(void)
-// {
-// 	printf("%s\n", ft_strjoin("tripouille", "42"));
-// 	printf("%s\n", ft_strjoin("", "42"));
-// 	printf("%s\n", ft_strjoin("", ""));
-// 	printf("%d\n", strjoin("tripouille", "42"));
-// 	printf("%d\n", strjoin("", "42"));
-// 	printf("%d\n", strjoin("", ""));
-// 	return (0);
-// }
