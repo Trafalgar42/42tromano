97a725b90dc6fc7e45744b098ec2c9d39b93bebb
Thom <romanothomas98@gmail.com>
2021-11-25 17:40:40 +0100

    oulah

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,10 +6,34 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:00:17 by tromano           #+#    #+#             */
-/*   Updated: 2021/11/24 17:00:30 by tromano          ###   ########.fr       */
+/*   Updated: 2021/11/25 17:35:14 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd);
+char	*get_next_line(int fd)
+{
+	t_read	*tab;
+	size_t	max;
+	size_t	i;
+	char	*read_line;
+
+	i = 0;
+	max = ft_strlen(tab->line);
+	while (tab->line[i])
+	{
+		if (tab->line[i] == '\n')
+		{
+			read_line = ft_substr(tab->line, 0, i);
+			tab->reste = ft_substr(tab->line, i + 1, max);
+			tab->count = i + 1;
+		}
+		if (tab->line[i] == '\0')
+		{
+			read_line = ft_substr(tab->line, 0, i);
+			tab->reste = NULL;
+			tab->count = i;
+		}
+	}
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,17 +6,30 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:59:03 by tromano           #+#    #+#             */
-/*   Updated: 2021/11/24 16:59:53 by tromano          ###   ########.fr       */
+/*   Updated: 2021/11/25 17:22:12 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stddef.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+typedef struct s_line
+{
+	size_t	count;
+	char	*reste;
+	int		fd;
+	char	line[BUFFER_SIZE];
+}	t_read;
 
 
 
 
 
-
-# endif
+#endif
