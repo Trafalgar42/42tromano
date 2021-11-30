2a49cec9200f5cfa0fe9be0a8f0d68beec048051
Thom <romanothomas98@gmail.com>
2021-11-30 19:07:14 +0100

    sdfdva

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,29 +6,11 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:20:15 by tromano           #+#    #+#             */
-/*   Updated: 2021/11/25 17:45:41 by tromano          ###   ########.fr       */
+/*   Updated: 2021/11/30 18:13:17 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	j;
-	size_t	i;
-
-	i = ft_strlen(src);
-	if (dstsize == 0)
-		return (i);
-	j = 0;
-	while (j < dstsize - 1 && src[j])
-	{
-		dst[j] = src[j];
-		j++;
-	}
-	dst[j] = '\0';
-	return (i);
-}
 
 int	ft_strlen(const char *str)
 {
@@ -42,54 +24,17 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_joint(char *str, char c)
 {
-	size_t	dlen;
-	char	*d;
+	char	*new;
+	int		i;
+	int		len;
 
-	if (!s)
-		return (NULL);
-	if (start > (unsigned int)ft_strlen(s))
-		return (ft_strdup(""));
-	dlen = ft_strlen(s) - start;
-	if (len < dlen)
-		dlen = len;
-	else if (dlen < 0)
-		dlen = 0;
-	d = malloc(sizeof(char) * (dlen + 1));
-	if (!d)
-		return (NULL);
-	ft_strlcpy(d, s + start, dlen + 1);
-	return (d);
-}
-
-char	*ft_strdup(const char *s)
-{
-	char	*dest;
-
-	if (*s == 0 && s == 0)
-		return (0);
-	dest = 0;
-	if (dest < s)
+	i = 0;
+	len = ft_strlen(str) + 2;
+	new = malloc(sizeof(len));
+	while (c)
 	{
-		dest = malloc((sizeof(char) * ft_strlen(s)) + 1);
-		if (!dest)
-			return (0);
-		return (dest = ft_strcpy(dest, (char *)s));
+		
 	}
-	return (0);
-}
-
-char	*ft_strcpy(char *dest, char *s)
-{
-	int	c;
-
-	c = 0;
-	while (s[c] != '\0')
-	{
-		dest[c] = s[c];
-		c++;
-	}
-	dest[c] = '\0';
-	return (dest);
 }
