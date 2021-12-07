98f8161374b4ae8ba0fab9d201878a302c5e71c1
Thom <romanothomas98@gmail.com>
2021-12-07 12:07:46 +0100

    jhgcjyfc

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:00:17 by tromano           #+#    #+#             */
-/*   Updated: 2021/12/01 15:14:39 by tromano          ###   ########.fr       */
+/*   Updated: 2021/12/06 12:48:19 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 char	*get_next_line(int fd)
 {
-	static t_read	*tab;
+	static t_read	tab = {.fd = -1};
 	char			*str;
 	char			c;
 
-	tab->fd = fd - 1;
-	if (fd != tab->fd)
-		if (!ft_init(tab, fd))
+	str = 0;
+	if (fd != tab.fd)
+		if (!ft_init(&tab, fd))
 			return (0);
-	c = ft_check(tab);
+	c = ft_check(&tab);
 	while (c)
 	{
 		str = ft_joint(str, c);
 		if (c == '\n')
 			return (str);
-		c = ft_check(tab);
+		c = ft_check(&tab);
 	}
 	return (str);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: tromano <tromano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:20:15 by tromano           #+#    #+#             */
-/*   Updated: 2021/12/01 15:08:05 by tromano          ###   ########.fr       */
+/*   Updated: 2021/12/01 21:29:24 by tromano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,26 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-char	*ft_joint(char *str, char c)
+char	*ft_joint(char *s, char c)
 {
 	char	*str1;
 	int		i;
 	int		len;
 
 	i = 0;
-	len = ft_strlen(str);
-	str1 = malloc(sizeof(len + 2));
-	while (*str)
+	len = ft_strlen(s);
+	str1 = malloc(len + 2);
+	if (!str1)
+		return (0);
+	while (i < len)
 	{
-		str1[i] = str[i];
+		str1[i] = s[i];
 		i++;
 	}
 	str1[i] = c;
 	str1[i + 1] = 0;
-	if (str)
-		free(str);
+	if (s)
+		free(s);
 	return (str1);
 }
 
@@ -50,7 +52,7 @@ int	ft_init(t_read *tab, int fd)
 	tab->fd = fd;
 	tab->pos = 0;
 	tab->max = read(tab->fd, tab->str, BUFFER_SIZE);
-	if (tab->max < 0)
+	if (tab->max <= 0)
 		return (0);
 	else
 		return (1);
@@ -60,7 +62,7 @@ char	ft_check(t_read *tab)
 {
 	char	res;
 
-	if (tab->max >= tab->pos)
+	if (tab->max <= tab->pos)
 	{
 		tab->pos = 0;
 		tab->max = read(tab->fd, tab->str, BUFFER_SIZE);
